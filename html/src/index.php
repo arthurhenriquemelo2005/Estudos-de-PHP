e2417b0f599ecaaf7aa56bca76be21a9e382c974
Arthur Ribeiro <arthurhenriquemelo2005@gmail.com>
2026-09-25 16:46:45 -0300

    Adicionando o formulario em PHP

--- html/src/index.php
+++ html/src/index.php
@@ -1,5 +1,12 @@
-<?php 
+<?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
 
+    echo "Cadastro realizado com sucesso!";
+    echo "<br>Nome: " . $nome;
+    echo "<br>E-mail: " . $email;
+}
 
 ?>
